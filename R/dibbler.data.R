25c977b4cf48eeac8424f5011b3a7c37027936fe
Thibaut Jombart <thibautjombart@gmail.com>
2016-02-16 12:14:10 +0000

    functions are now templates

--- R/dibbler.data.R
+++ R/dibbler.data.R
@@ -1,90 +1,35 @@
-#' Process input data for vimes
+#' Process input data for dibbler
 #'
-#' This function takes a series of 'dist' objects (or objects which can be converted to 'dist', and ensures that entries match in all objects, adding NAs where necessary. The total number of cases and labels are returned as attributes.
+#' This function ...
 #'
 #' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
 #'
-#' @param ... a series (alternatively, a list) of matrices or dist objects serving as input.
-#' @param na.rm a logical indicating if cases with missing data should be removed.
+#' @param graph a input for buidling a graph (see details)
+#' @param tree a phylogenetic tree with \code{phylo} class
+#' @param ... a list containing inputs for  \code{\link{dibbler}}
 #'
 #' @export
 #' @importFrom stats as.dist
 #'
-#' @return a list of dist objects with matching entries, with attributes: 'labels' (labels of the cases) and 'N' (number of cases)
+#' @return a list of data suitable for input for \code{\link{dibbler}}
 #'
 #' @examples
-#' x1 <- c(0,1,3)
-#' x2 <- c(2,5)
-#' names(x1) <- letters[1:3]
-#' names(x2) <- c('a', 'r')
-#' D1 <- dist(x1)
-#' D2 <- dist(x2)
-#' out <- vimes.data(D1, D2)
-#' out
 #'
-vimes.data <- function(..., na.rm=FALSE){
+dibbler.data <- function(graph=NULL, tree=NULL, ...){
     ## PROCESS INPUT ##
     ## extract data from list ##
     data <- list(...)
-    data.names <- names(data)
     if(length(data)==0L) stop("no data to process")
 
     ## escape if data has been processed already
-    if(inherits(data[[1]],"vimes.input")) return(data[[1]])
+    if(inherits(data[[1]],"dibbler.input")) return(data[[1]])
 
     ## if first item is a list, use it as input
     if(is.list(data[[1]])) data <- data[[1]]
 
-    ## handle NAs ##
-    if(na.rm){
-        for(i in seq_along(data)){
-            temp <- as.matrix(data[[i]])
-            to.keep <- !apply(is.na(temp), 1, all)
-            data[[i]] <- as.dist(temp[to.keep, to.keep])
-        }
-    }
-
-    ## convert data to dist ##
-    data <- lapply(data, as.dist)
-
-    ## add labels if needed ##
-    for(i in seq_along(data)){
-        if(is.null(labels(data[[i]]))) attr(data[[i]], "Labels") <- 1:attr(data[[i]], "Size")
-    }
-
-    ## MATCH ORDERING/FILL GAPS ##
-    ## get list of all labels ##
-    if(na.rm){
-        all.labels <- Reduce(intersect, lapply(data, attr, "Labels"))
-    } else {
-        all.labels <- unique(unlist(lapply(data, attr, "Labels")))
-    }
-    N <- length(all.labels)
-    if(N<2){
-        warning("Data contain less than 2 cases - try 'na.rm=FALSE'?")
-        return(NULL)
-    }
-
-    ## create model matrix ##
-    out <- list()
-    empty.mat <- matrix(NA_real_, ncol=N, nrow=N)
-    rownames(empty.mat) <- colnames(empty.mat) <- all.labels
-
-    ## fit data into model matrix
-    for(i in seq_along(data)){
-        temp <- as.matrix(data[[i]])
-        temp <- temp[rownames(temp) %in% all.labels, colnames(temp) %in% all.labels, drop=FALSE]
-        temp.lab <- rownames(temp)
-        out[[i]] <- empty.mat
-        out[[i]][temp.lab, temp.lab] <- temp
-        out[[i]] <- as.dist(out[[i]])
-    }
 
     ## RETURN OUTPUT ##
-    names(out) <- data.names
-    attr(out, "labels") <- all.labels
-    attr(out, "N") <- length(all.labels)
-
-    class(out) <- c("list", "vimes.input")
+    out <- list()
+    class(out) <- c("list", "dibbler.input")
     return(out)
-} # end vimes.data
+} # end dibbler.data
--- R/dibbler.pal.R
+++ R/dibbler.pal.R
@@ -1,23 +1,23 @@
-#' Color palettes used in vimes
+#' Color palettes used in dibbler
 #'
-#' These functions are color palettes used in vimes.
+#' These functions are color palettes used in dibbler.
 #'
 #' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
 #'
 #' @param n a number of colors
 #'
 #' @rdname palettes
-#' @aliases palettes vimes.pal1 vimes.pal2
+#' @aliases palettes dibbler.pal1 dibbler.pal2
 #'
 #' @export
 #' @importFrom grDevices colorRampPalette
 #'
 #' @examples
 #'
-#' plot(1:4, cex=8, pch=20, col=vimes.pal1(4), main="vimes.pal1")
-#' plot(1:20, col=vimes.pal1(20), pch=20, cex=6, main="vimes.pal1")
-#' plot(1:10, col=vimes.pal2(10), pch=20, cex=6, main="vimes.pal2")
-vimes.pal1 <- function(n){
+#' plot(1:4, cex=8, pch=20, col=dibbler.pal1(4), main="dibbler.pal1")
+#' plot(1:20, col=dibbler.pal1(20), pch=20, cex=6, main="dibbler.pal1")
+#' plot(1:10, col=dibbler.pal2(10), pch=20, cex=6, main="dibbler.pal2")
+dibbler.pal1 <- function(n){
     colors <- c("#ff6666", "#666699")
     if(n<length(colors)) {
         return(colors[seq_len(n)])
@@ -29,7 +29,7 @@ vimes.pal1 <- function(n){
 
 #' @rdname palettes
 #' @export
-vimes.pal2 <- function(n){
+dibbler.pal2 <- function(n){
     colors <- c("#ccddff", "#79d2a6", "#ffb3b3",
                 "#a4a4c1","#ffcc00", "#ff9f80",
                 "#ccff99", "#df9fbf","#ffcc99",
