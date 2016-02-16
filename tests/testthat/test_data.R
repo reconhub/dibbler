88adee59fb5cbe4520fe16146740706b4ef13ab5
thibautjombart <thibautjombart@gmail.com>
2016-02-16 12:21:28 +0000

    adding tets

---
+++ tests/testthat/test_data.R
@@ -0,0 +1,20 @@
+context("Test input data processing")
+
+
+## test data ##
+test_that("test: data are processed fine", {
+    ## skip on CRAN
+    skip_on_cran()
+    rm(list=ls())
+
+    ## generate data
+    out <- dibbler.data()
+
+    ## check output shape
+
+    ## check attributes
+
+    ## round trip
+
+})
+
