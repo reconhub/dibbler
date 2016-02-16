cff5b1b78f08ef64def2f9d207489dc2b3c0c18c
thibautjombart <thibautjombart@gmail.com>
2016-02-16 16:22:09 +0000

    conversion tests OK

--- tests/testthat/test_conversions.R
+++ tests/testthat/test_conversions.R
@@ -9,17 +9,18 @@ test_that("test: network <-> igraph", {
 
     ## generate data
     set.seed(1)
-    g1 <- sample_pa_age(10, pa.exp=1, aging.exp=0, aging.bin=1000)
-    g1 <- graph.data.frame(as_data_frame(g1)) # need to ensure sorting
-    
-    net <- igraph2network(g1)
-    g2 <- network2igraph(net)
-    
-    ## check output shape
-    
-    ## check attributes
+    g0 <- igraph::sample_pa_age(10, pa.exp=1, aging.exp=0, aging.bin=1000)
+    n1 <- igraph2network(g0)
+    g1 <- network2igraph(n1)
 
-    ## round trip
+    ## check classes
+    expect_is(n1,"network")
+    expect_is(g1,"igraph")
 
+    ## compare edge lists
+    n1.vec <- sort(apply(network2data.frame(n1),1,paste, collapse="->"))
+    g1.vec <- sort(apply(igraph2data.frame(g1),1,paste, collapse="->"))
+    names(n1.vec) <- names(g1.vec) <- NULL
+    expect_identical(g1.vec, n1.vec)
 })
 
