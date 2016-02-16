9d1308dfe73160a2c3315fb90071c3084431380b
thibautjombart <thibautjombart@gmail.com>
2016-02-16 15:35:52 +0000

    adding functions

--- R/conversions.R
+++ R/conversions.R
@@ -1,9 +1,12 @@
 
 
-#' Convert a directed graphs between network and igraph
+#' Convert a directed graphs from network to igraph
+#'
+#' The function convert a directed graph with the network format into an igraph object.
 #'
 #' @export
 #' @importFrom igraph graph.data.frame
+#' @importFrom network as.edgelist
 #'
 #' @param x a \code{network} object.
 #'
@@ -24,4 +27,33 @@ network2igraph <- function(x){
     out <- igraph::graph.data.frame(as.data.frame(out))
 
     return(out)
-}
+} # end network2igraph
+
+
+
+
+
+
+
+#' Convert a directed graphs from igraph to network
+#'
+#' The function convert a directed graph with the igraph format into an network object.
+#'
+#' @export
+#' @importFrom network graph.data.frame network
+#' @importFrom igraph as.edgelist as_data_frame
+#'
+#' @param x a \code{igraph} object.
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+igraph2network <- function(x){
+    ## get edge list
+    temp <- igraph::as_data_frame(out)
+
+
+    ## restore labels
+    out <- network::network(temp)
+
+    return(out)
+} # end igraph2network
