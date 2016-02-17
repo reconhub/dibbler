3f33c7ca514015c1b13df85cbc9cafaba6b9d768
thibautjombart <thibautjombart@gmail.com>
2016-02-17 19:27:47 +0000

    updated doc

--- R/data.R
+++ R/data.R
@@ -1,7 +1,6 @@
+#' Salmonella Enteritidis P59 outbreak
 #'
-#'
-#' This dataset is a small simulated outbreak used to illustrate outbreaker.
-#' It was generated using \code{simOutbreak}.
+#' This dataset covers a food-borne outbreak of Salmonella Enteritidis PT59 investigated by Public Health England. The data includes a distribution network, and genetic clusters of bacteria isolated in contaminated patients. The data is anonymised: identifiers of the food distribution network have been hashed.
 #'
 #' @aliases fakeOutbreak
 #' @docType data
