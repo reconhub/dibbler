698712a58378bb52811d2d2e92a23cfbd1ad61cf
thibautjombart <thibautjombart@gmail.com>
2016-02-17 16:06:12 +0000

    adding doc for data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#'
+#'
+#' This dataset is a small simulated outbreak used to illustrate outbreaker.
+#' It was generated using \code{simOutbreak}.
+#'
+#' @aliases fakeOutbreak
+#' @docType data
+#' @source Gastrointestinal Bacteria Reference Unit, Public Health England
+#' @author Thomas Inns, Philip Ashton, Tim Dallman, Roberto Vivancos
+#' @keywords datasets
+#'
+"Salmonella"
+
+
