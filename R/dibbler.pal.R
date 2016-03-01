e5c1f67c45748eeb1854ba9b37672b835c1f7fb4
Thibaut Jombart <thibautjombart@gmail.com>
2016-03-01 12:27:04 +0000

    adding checks to palettees

--- R/dibbler.pal.R
+++ R/dibbler.pal.R
@@ -18,6 +18,7 @@
 #' plot(1:20, col=dibbler.pal1(20), pch=20, cex=6, main="dibbler.pal1")
 #' plot(1:10, col=dibbler.pal2(10), pch=20, cex=6, main="dibbler.pal2")
 dibbler.pal1 <- function(n){
+    if(!is.numeric(n)) stop("n is not a number")
     colors <- c("#cc6666", "#ff8566", "#ffb366","#33cccc",
                 "#85e0e0", "#adc2eb", "#9f9fdf","#666699")
     return(colorRampPalette(colors)(n))
@@ -28,6 +29,7 @@ dibbler.pal1 <- function(n){
 #' @rdname palettes
 #' @export
 dibbler.pal2 <- function(n){
+    if(!is.numeric(n)) stop("n is not a number")
     colors <- c("#ccddff", "#79d2a6", "#ffb3b3",
                 "#a4a4c1","#ffcc00", "#ff9f80",
                 "#ccff99", "#df9fbf","#ffcc99",
