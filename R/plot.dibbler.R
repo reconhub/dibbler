96fad44da6a27bee0cc10a8d69d18c6e01dfd55c
thibautjombart <thibautjombart@gmail.com>
2017-05-10 19:37:16 +0100

    unfinished stuff

--- R/plot.dibbler.R
+++ R/plot.dibbler.R
@@ -1,86 +1,33 @@
 #' Plot input data for dibbler
 #'
-#' This function plots dibbler's input data as a network, using different symbols and colors for different types of nodes. It uses the package \code{network}.
+#' This function plots dibbler's input data as a network, using different
+#' symbols and colors for different types of nodes. It uses the package
+#' \code{network}.
 #'
 #' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
 #'
-#' @param x an input dataset as returned by \code{dibbler.data}
-#' @param y unused argument, for compatibility with the \code{plot} generic
-#' @param cex a size factor for the nodes of the network
-#' @param lab.cex a size factor for the tip annotations (genetic clusters)
-#' @param color.internal a logical indicating whether internal nodes and their descending edges should be colored
-#' @param col.pal1 a color palette to be used for the genetic clusters (tips)
-#' @param col.pal2 a color palette to be used for identifying internal nodes
-#' @param ... further arguments to be passed to \code{plot.network}
+#' @importFrom graphics plot
+#' 
+#' @importFrom magrittr "%>%"
+#' 
 #'
 #' @export
-#' @importFrom network network.vertex.names
-#' @importFrom graphics plot
-#'
-#' @return the same output as \code{plot.network}
-#'
-#' @seealso \code{\link[network]{plot.network}} in the package \code{network}.
-#'
-#' @examples
-#'
-#'
-plot.dibbler.data <- function(x, y=NULL, cex=1, lab.cex=1, color.internal=TRUE,
-                               col.pal1=dibbler.pal1, col.pal2=dibbler.pal2, ...){
-    ## convert input graph to network
-    net <- igraph2network(x$graph)
-    net.df <- network2data.frame(net)
-    v.names <- network.vertex.names(net)
-    id.terminal <- which(!v.names %in% net.df$from)
-    id.basal <- which(!v.names %in% net.df$to)
-    id.internal <- which(!v.names %in% v.names[id.terminal])
 
-    ## basic variables
-    N <- length(network::network.vertex.names(net))
-    N.internal <- length(id.internal)
-    N.tips <- length(id.terminal)
-    K <- length(levels(x$group))
+plot.dibbler <- function(x, y = NULL, ...){
 
-    ## vertex labels
-    temp <- x$group
-    v.lab <- rep("", N)
-    names(v.lab) <- v.names
-    v.lab[names(x$group)] <- as.character(x$group)
+    df_to_add <- data.frame(id = names(x$node_type),
+                            group = factor(x$node_type))
 
-    ## internal node colors
-    v.col <- rep("grey",.5, N)
-    names(v.col) <- v.names
-    if(color.internal){
-        v.col[id.internal] <- col.pal2(N.internal)
-    }
+    x$linelist <- merge(x$linelist, df_to_add,
+                        by = "id", all = TRUE)
+    
+    out <- getS3method("plot", "epicontacts")(x, y, ...)
 
-    ## group colors
-    grp.col <- col.pal1(K)
-
-    ## tip colors
-    v.col[names(x$group)] <- grp.col[x$group]
-
-    ## vertex shapes
-    v.sides <- rep(50, N)
-    v.sides[id.terminal] <- 3
-    v.sides[id.basal] <- 6
-    v.lwd <- rep(1,N)
-    v.lwd[id.basal] <- 3
-
-    ## vertex sizes
-    v.size <- sapply(v.names, function(e) sum(e == net.df$from))
-    v.size <- 1 + sqrt(v.size) * (cex/2)
-    v.size[id.terminal] <- 1
-
-    ## edge color
-    e.df <- as.matrix(net, matrix.type="edgelist")
-    e.col <- v.col[attr(e.df, "vnames")[e.df[,1]]]
-
-    ## make plot
-    out <- plot(net, label=v.lab, label.cex=lab.cex,
-         edge.col=e.col, vertex.col=v.col,
-         label.col=v.col, vertex.cex=v.size,
-         vertex.sides=v.sides,
-         vertex.lwd=v.lwd, ...)
-
-    return(invisible(out))
-} # end plot.dibbler.data
+    out <- out %>%
+        visNetwork::visGroups(groupname = "terminal",
+                              shape = "icon",
+                              list(code = "f007", color = "red")) %>%
+                                  visNetwork::addFontAwesome()
+    
+    return(out)
+} # end plot.dibbler
